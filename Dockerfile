f50b90803966fa5ab5ed5f61f3b7c20a984ff6af
Evgeny_Ch <gek_sh@mail.ru>
2024-08-11 15:34:55 +0300

    Added environment variable PORT

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+﻿FROM gradle:8.5-jdk21
+
+WORKDIR /app
+
+COPY . /app
+
+RUN gradle installDist
+
+CMD ./build/install/app/bin/app
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+}
